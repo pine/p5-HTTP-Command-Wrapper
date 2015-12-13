7f0cbc0776059e065c3b6356d32d088afab322ac
Pine Mizune <pinemz@gmail.com>
2015-12-13 20:05:12 +0900

    chore(repository): initial commit :sparkles:

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+HTTP::Command::Wrapper - It's new $module
+
+# SYNOPSIS
+
+    use HTTP::Command::Wrapper;
+
+# DESCRIPTION
+
+HTTP::Command::Wrapper is ...
+
+# LICENSE
+
+Copyright (C) Pine Mizune.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Pine Mizune <pinemz@gmail.com>
---
+++ lib/HTTP/Command/Wrapper.pm
@@ -0,0 +1,39 @@
+package HTTP::Command::Wrapper;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+HTTP::Command::Wrapper - It's new $module
+
+=head1 SYNOPSIS
+
+    use HTTP::Command::Wrapper;
+
+=head1 DESCRIPTION
+
+HTTP::Command::Wrapper is ...
+
+=head1 LICENSE
+
+Copyright (C) Pine Mizune.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Pine Mizune E<lt>pinemz@gmail.comE<gt>
+
+=cut
+
