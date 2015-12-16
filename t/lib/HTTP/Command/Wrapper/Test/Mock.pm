80b76d68310415452fd2914c290bed5a91e7c50b
Pine Mizune <pinemz@gmail.com>
2015-12-17 01:06:30 +0900

    refactor(test): refactor :sparkles:

--- t/lib/HTTP/Command/Wrapper/Test/Mock.pm
+++ t/lib/HTTP/Command/Wrapper/Test/Mock.pm
@@ -13,10 +13,9 @@ our @EXPORT = qw/create_binary_mock/;
 sub create_binary_mock (&) {
     my $code      = shift;
     my $mock_path = File::Spec->rel2abs(File::Spec->catfile(dirname(__FILE__), qw/.. .. .. .. .. data bin/));
-    my $sys_path  = defined $ENV{COMSPEC} ? dirname($ENV{COMSPEC}) : undef;
     my $path      = $mock_path;
 
-    $path .= $Config{path_sep}.$sys_path if defined $sys_path;
+    $path .= $Config{path_sep}.dirname($ENV{COMSPEC}) if defined $ENV{COMSPEC};
     local $ENV{PATH} = $path;
 
     return $code->();
